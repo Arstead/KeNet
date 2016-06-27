879b9928ee8d4be3848085432e866c5c2bd7d039
Wang Shupeng <xsw200831720@gmail.com>
2016-06-27 23:12:10 +0800

    添加softmax激活函数

--- KeNet/core/activation_function.py
+++ KeNet/core/activation_function.py
@@ -1,14 +1,25 @@
+from __future__ import division
 import numpy as np
+
 
 def logistic(x):
     return 1.0 / (1 + np.exp(-x))
 
+
 def logistic_d(x):
     l = logistic(x)
-    return np.array(list(map(lambda x : x * (1 - x), l)))
+    return np.array(list(map(lambda x: x * (1 - x), l)))
+
 
 def linear(x):
     return x
 
+
 def linear_d(x):
     return np.ones(x.shape)
+
+
+def softmax(x):
+    y = np.exp(x)
+    s = np.sum(y)
+    return y / s
