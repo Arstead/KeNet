07d35f17af33cef9f3d8f1b5b3d6aadb4408e835
Wang Shupeng <xsw200831720@gmail.com>
2016-06-30 01:51:00 +0800

    将功能函数由向量操作变为矩阵操作

--- KeNet/core/error_function.py
+++ KeNet/core/error_function.py
@@ -1,20 +1,25 @@
 from __future__ import division
 import numpy as np
 
-def mean_squared_error(x, y):
-    return np.sum(np.power(y - x, 2))
+def mean_squared_error(A, B):
+    assert A.shape != B.shape, 'Shape of Input data should be same ' \
+                               'when you want to get precision.'
+    error_counter = 0.0
+    for i, j in zip(A, B):
+        error_counter += np.sum(np.power(i - j, 2))
+    return error_counter
 
 
-def precision(x, y):
-    assert x.shape != y.shape, 'Shape of Input data should be same ' \
+def precision(A, B):
+    assert A.shape != B.shape, 'Shape of Input data should be same ' \
                                'when you want to get precision.'
     right_counter = 0
-    for i, j in zip(x, y):
+    for i, j in zip(A, B):
         i_idx = np.argwhere(i == 1)
         j_idx = np.argwhere(j == 1)
         assert len(i_idx) == 1, 'Only predict class number can be seted as one in Input x'
         assert len(j_idx) == 1, 'Only predict class number can be seted as one in Input Y'
         if j_idx == i_idx:
             right_counter += 1
-    return right_counter / len(x)
+    return right_counter / len(A)
 
