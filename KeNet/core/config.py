f6c44fd641b68b5248d3dc0d14729c1700dbdada
Wang Shupeng <xsw200831720@gmail.com>
2016-06-29 15:25:39 +0800

    配置文件，包含一些全局的参数

---
+++ KeNet/core/config.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+"""
+    this file is created for some global parameter
+"""
+
+
+
+
+# For dataset
+data_base_path = r'D:\python_workstation\KeNet-0.1.1\KeNet\dataset\data'
