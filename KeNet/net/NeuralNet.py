73febfa641f4a1ae3877d5ff7b43fc2515226018
Wang Shupeng <xsw200831720@gmail.com>
2016-06-30 01:49:17 +0800

    将函数由向量操作修改成矩阵操作，但是存在bug，cols 51

--- KeNet/net/NeuralNet.py
+++ KeNet/net/NeuralNet.py
@@ -3,6 +3,7 @@
 from ..layers import *
 from .Net import *
 from ..core import error_function as EF
+from ..dataset.utilor import to_labels
 import numpy as np
 
 
@@ -12,25 +13,31 @@ class NeuralNet(Net):
         self._is_back_propagation = False
         self._is_feedforward = False
 
-    def _feedforward(self, x):
+    def _feedforward(self, X):
+        assert X.ndim == 2, '_feedforward:Input data must be an M x N matrix. ' \
+                            'M is num of samples, N is num of features for each sample'
+        Y = np.zeros([len(X), self.layers[-1].nodes_num])
+
         assert self.is_complie, 'complie network before you use it.'
-        self.layers[0].output = x
+        self.layers[0].output = X
         for layer_idx in range(1, len(self.layers)):
             self.layers[layer_idx].input = np.dot(self.layers[layer_idx - 1].output, self.layers[layer_idx].weights) + \
-                                           self.layers[layer_idx].bias
+                                            self.layers[layer_idx].bias
             self.layers[layer_idx].output = self.layers[layer_idx].activation(self.layers[layer_idx].input)
+
+            Y = self.layers[-1].output
         self._is_feedforward = True
-        return self.layers[-1].output
+        return Y
 
-    def _back_propagation(self, x, y, delta=None):
+    def _back_propagation(self, X, Y, delta=None):
 
-        assert type(x) is np.ndarray, 'input of data x need to be type of ndarry'
-        assert type(x) is np.ndarray, 'labels data y need to be type of ndarry'
+        assert type(X) is np.ndarray, 'input of data x need to be type of ndarry'
+        assert type(X) is np.ndarray, 'labels data y need to be type of ndarry'
         if delta:
             assert type(delta) is np.ndarray, 'data delta need to be type of ndarry'
-            self._feedforward(x)
+            self._feedforward(X)
         else:
-            delta = y - self._feedforward(x)
+            delta = Y - self._feedforward(X)
         self.layers[-1].delta = delta
         for layer_idx in range(len(self.layers) - 2, 0, -1):
             self.layers[layer_idx].delta = np.dot(self.layers[layer_idx + 1].weights,
@@ -52,11 +59,17 @@ class NeuralNet(Net):
     def _computer_error(self, x, y):
         return EF.mean_squared_error(x, y)
 
-    def _computer_precision(self, x, y):
-        return EF.precision(x, y)
+    def _computer_precision(self, A, B):
+        return EF.precision(A, B)
 
-    def predict(self, x):
-        return self._feedforward(x)
+    def _set_class(self, X):
+        Y = np.zeros(X.shape)
+        for idx, x in enumerate(X):
+            Y[idx, np.argmax(x)] = 1
+        return Y
+
+    def predict(self, X):
+        return self._feedforward(X)
 
     def train(self, train_data, train_labels, validation_data=None, validation_labels=None):
         assert type(train_data) is np.ndarray, 'input of train data need to be type of ndarry'
@@ -77,27 +90,34 @@ class NeuralNet(Net):
         for i in range(self.epoch):
             print('epoch %d:' % i, end='  ')
             # update weight
-            for sample_idx in range(train_sample_num):
-                self._back_propagation(train_data[sample_idx], train_labels[sample_idx])
-                self._update_weight()
+            self._back_propagation(train_data, train_labels)
+            self._update_weight()
 
             sum_train_error = 0.0
-            # compute error
-            for sample_idx in range(train_sample_num):
-                output = self._feedforward(train_data[sample_idx])
-                sum_train_error = sum_train_error + self._computer_error(output, train_labels[sample_idx])
-            print('train error : %0.4f' % sum_train_error, end='  ')
+            # compute train MSE
+            O = self._feedforward(train_data)
+            MSE = self._computer_error(O, train_labels)
+            print('train error : %0.4f' % MSE, end='  ')
 
-            # compute validation error
+            # compute train precision
+            O = self._feedforward(train_data)
+            C = self._set_class(O)
+            acc = self._computer_precision(C, train_labels)
+            print('train acc : %0.4f' % acc, end='  ')
+
+
             if validation_data is not None:
-                validation_sample_num = validation_data.shape[0]
-                sum_validation_error = 0.0
-                for sample_idx in range(validation_sample_num):
-                    output = self._feedforward(validation_data[sample_idx])
-                    sum_validation_error = sum_validation_error + \
-                                           self._computer_error(output, validation_labels[sample_idx])
-                print('validation error : %0.4f' % sum_validation_error, end='  ')
-            print('')
+                # compute validation MSE
+                O = self._feedforward(validation_data)
+                MSE = self._computer_error(O, validation_labels)
+                print('validation error : %0.4f' % MSE, end='  ')
+
+                # compute validation precision
+                O = self._feedforward(validation_data)
+                C = self._set_class(O)
+                acc = self._computer_precision(C, validation_labels)
+                print('train acc : %0.4f' % acc, end='  ')
+                print('')
 
     def add_layer(self, layer):
         self.layers.append(layer)
