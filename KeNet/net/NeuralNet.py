a812a80b62ddb9bf144b25ab0462f4262b8714e0
Wang Shupeng <xsw200831720@gmail.com>
2016-06-30 15:03:57 +0800

    修复矩阵操作出现的bug，添加batch_size

--- KeNet/net/NeuralNet.py
+++ KeNet/net/NeuralNet.py
@@ -25,7 +25,7 @@ class NeuralNet(Net):
                                             self.layers[layer_idx].bias
             self.layers[layer_idx].output = self.layers[layer_idx].activation(self.layers[layer_idx].input)
 
-            Y = self.layers[-1].output
+        Y = self.layers[-1].output
         self._is_feedforward = True
         return Y
 
@@ -47,20 +47,37 @@ class NeuralNet(Net):
 
     def _update_weight(self):
         assert self._is_back_propagation, 'do back propagation before update weight'
+        samples_num = len(self.layers[0].output)
         for layer_idx in range(1, len(self.layers)):
-            self.layers[layer_idx].weights += self.learning_rate * np.outer(
-                self.layers[layer_idx - 1].output,
+            self.layers[layer_idx].weights += (1 / samples_num) * self.learning_rate * np.dot(
+                self.layers[layer_idx - 1].output.T,
                 self.layers[layer_idx].delta
             )
-            self.layers[layer_idx].bias += self.learning_rate * self.layers[layer_idx].delta
+            self.layers[layer_idx].bias += (1 / samples_num) * self.learning_rate * np.sum(self.layers[layer_idx].delta, axis=0)
         self._is_back_propagation = False
         self._is_feedforward = False
 
-    def _computer_error(self, x, y):
-        return EF.mean_squared_error(x, y)
+    def _computer_error(self, A, B):
+        assert A.shape == B.shape, 'Shape of Input data should be same ' \
+                                   'when you want to get precision.'
+
+        error_counter = 0.0
+        for i, j in zip(A, B):
+            error_counter += EF.mean_squared_error(i, j)
+        return error_counter / len(A)
 
     def _computer_precision(self, A, B):
-        return EF.precision(A, B)
+        assert A.shape == B.shape, 'Shape of Input data should be same ' \
+                                   'when you want to get precision.'
+        right_counter = 0
+        for i, j in zip(A, B):
+            i_idx = np.argwhere(i == 1)
+            j_idx = np.argwhere(j == 1)
+            assert len(i_idx) == 1, 'Only predict class number can be seted as one in Input x'
+            assert len(j_idx) == 1, 'Only predict class number can be seted as one in Input Y'
+            if j_idx == i_idx:
+                right_counter += 1
+        return right_counter / len(A)
 
     def _set_class(self, X):
         Y = np.zeros(X.shape)
@@ -86,18 +103,22 @@ class NeuralNet(Net):
             assert validation_data.shape[0] == validation_labels.shape[
                 0], 'number of validation data must be equal to number of train labels'
 
-        train_sample_num = train_data.shape[0]
+        train_samples_num = train_data.shape[0]
         for i in range(self.epoch):
             print('epoch %d:' % i, end='  ')
-            # update weight
-            self._back_propagation(train_data, train_labels)
-            self._update_weight()
 
-            sum_train_error = 0.0
+            # update weight
+            batch_num = train_samples_num // self.batch_size
+            for batch_idx in range(batch_num):
+                T = train_data[batch_idx * self.batch_size: (batch_idx + 1) * self.batch_size]
+                L = train_labels[batch_idx * self.batch_size: (batch_idx + 1) * self.batch_size]
+                self._back_propagation(T, L)
+                self._update_weight()
+
             # compute train MSE
             O = self._feedforward(train_data)
             MSE = self._computer_error(O, train_labels)
-            print('train error : %0.4f' % MSE, end='  ')
+            print('train mse : %0.4f' % MSE, end='  ')
 
             # compute train precision
             O = self._feedforward(train_data)
@@ -105,12 +126,11 @@ class NeuralNet(Net):
             acc = self._computer_precision(C, train_labels)
             print('train acc : %0.4f' % acc, end='  ')
 
-
             if validation_data is not None:
                 # compute validation MSE
                 O = self._feedforward(validation_data)
                 MSE = self._computer_error(O, validation_labels)
-                print('validation error : %0.4f' % MSE, end='  ')
+                print('validation mse : %0.4f' % MSE, end='  ')
 
                 # compute validation precision
                 O = self._feedforward(validation_data)
